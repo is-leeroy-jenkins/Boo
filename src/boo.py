abdecd7c029f7d39f0b072ec639e4d8743094d05
Terry D. Eppler <terryeppler@gmail.com>
2025-04-03 10:30:14 -0400

    updates

--- src/boo.py
+++ src/boo.py
@@ -79,6 +79,7 @@ class EndPoint( ):
 		self.text_generation = f'https://api.openai.com/v1/chat/completions'
 		self.image_generation = f'https://api.openai.com/v1/images/generations'
 		self.chat_completion = f'https://api.openai.com/v1/chat/completions'
+		self.responses = f'https://api.openai.com/v1/responses'
 		self.speech_generation = f'https://api.openai.com/v1/audio/speech'
 		self.translations = f'https://api.openai.com/v1/audio/translations'
 		self.assistants = f'https://api.openai.com/v1/assistants'
@@ -98,7 +99,7 @@ class EndPoint( ):
 		return [ 'base_url', 'text_generation', 'image_generation', 'chat_completions',
 		         'speech_generation', 'translations', 'assistants', 'transcriptions',
 		         'finetuning', 'embeddings', 'uploads', 'files', 'vector_stores',
-		         'get_data', 'dump' ]
+		         'responses', 'get_data', 'dump' ]
 	
 	
 	def get_data( self ) -> dict:
@@ -111,6 +112,7 @@ class EndPoint( ):
 		         'text_generation': self.text_generation,
 		         'image_generation': self.image_generation,
 		         'chat_completion': self.chat_completion,
+		         'responses': self.responses,
 		         'speech_generation': self.speech_generation,
 		         'translations': self.translations,
 		         'assistants': self.assistants,
@@ -1254,4 +1256,182 @@ class ChatLog(  ):
 	
 	'''
 	def __init__( self ):
-		self.messages = []
+		self.messages = [ GptMessage ]
+		
+		
+class Perceptron:
+	'''
+		Purpose
+		________
+		
+		Class to train models via fit function
+		
+		
+		Parameters
+		------------
+		eta : float
+		Learning rate (between 0.0 and 1.0)
+		n_iter : int
+		Passes over the training dataset.
+		random_state : int
+		Random number generator seed for random weight
+		initialization.
+		
+		
+		Attributes
+		-----------
+		w_ : 1d-array
+		Weights after fitting.
+		b_ : Scalar
+		Bias unit after fitting.
+		errors_ : list
+		Number of misclassifications (updates) in each epoch.
+	
+	
+	'''
+	def __init__( self, eta=0.01, n_iter=50, random_state=1 ):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.random_state = random_state
+	
+	
+	def fit( self, X, y ):
+		"""
+		
+			Purpose
+			_______
+			Fit training data.
+			
+			
+			Parameters
+			----------
+			X : {array-like}, shape = [n_examples, n_features]
+			Training vectors, where n_examples is the number of
+			examples and n_features is the number of features.
+			
+			y : array-like, shape = [n_examples]
+			Target values.
+			
+			Returns
+			-------
+			self : object
+		
+		"""
+		rgen = np.random.RandomState( self.random_state )
+		self.w_ = rgen.normal( loc=0.0, scale=0.01, size=X.shape[ 1 ] )
+		self.b_ = np.float_( 0. )
+		self.errors_ = [ ]
+		
+		for _ in range( self.n_iter ):
+			errors = 0
+			
+		for xi, target in zip( X, y ):
+			update = self.eta * ( target - self.predict( xi ) )
+			
+		self.w_ += update * xi
+		self.b_ += update
+		errors += int( update != 0.0 )
+		self.errors_.append( errors )
+		return self
+		
+		
+	def net_input( self, X ):
+		"""
+			Calculate net input
+		"""
+		return np.dot( X, self.w_ ) + self.b_
+	
+	
+	def predict( self, X ):
+		"""
+		Return class label after unit step
+		"""
+		return np.where( self.net_input( X ) >= 0.0, 1, 0 )
+
+
+class AdaptiveLinearNeuron( ):
+	"""
+		
+		Purpose
+		___________
+		Adaptive Linear Neuron classifier.
+		
+		Parameters
+		------------
+		eta : float
+		Learning rate (between 0.0 and 1.0)
+		n_iter : int
+		Passes over the training dataset.
+		random_state : int
+		Random number generator seed for random weight initialization.
+		
+		Attributes
+		-----------
+		w_ : 1d-array
+		Weights after fitting.
+		b_ : Scalar
+		Bias unit after fitting.
+		losses_ : list
+		Mean squared error loss function values in each epoch.
+	
+	"""
+	def __init__(self, eta=0.01, n_iter=50, random_state=1):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.random_state = random_state
+		
+	def fit(self, X, y):
+		""" Fit training data.
+
+			Parameters
+			----------
+			X : {array-like}, shape = [n_examples, n_features]
+			Training vectors, where n_examples
+			is the number of examples and
+			n_features is the number of features.
+			y : array-like, shape = [n_examples] Target values.
+	
+			Returns
+			-------
+			self : object
+		
+		"""
+		rgen = np.random.RandomState(self.random_state)
+		self.w_ = rgen.normal(loc=0.0, scale=0.01,
+		size=X.shape[1])
+		self.b_ = np.float_(0.)
+		self.losses_ = []
+		for i in range(self.n_iter):
+			net_input = self.net_input(X)
+			
+		output = self.activation(net_input)
+		errors = (y - output)
+		self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+		self.b_ += self.eta * 2.0 * errors.mean()
+		loss = ( errors**2 ).mean( )
+		self.losses_.append( loss )
+		return self
+
+	def net_input( self, X ):
+		"""
+			
+			Calculate net
+			
+		"""
+		return np.dot( X, self.w_ ) + self.b_
+
+	def activation( self, X) :
+		"""
+		
+			Compute linear activation
+		
+		"""
+		return X
+
+	def predict( self, X ):
+		"""
+			
+			Return class label after unit step
+		
+		"""
+		return np.where( self.activation( self.net_input( X ) ) >= 0.5, 1, 0 )
