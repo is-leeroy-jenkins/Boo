35ae0e561ea1d3903af8d48a9577ff3a92c97c49
Terry D. Eppler <terry.eppler@gmail.com>
2025-04-28 07:23:14 -0400

    updates

---
+++ main.py
@@ -0,0 +1,57 @@
+'''
+  ******************************************************************************************
+      Assembly:                Boo
+      Filename:                main.py
+      Author:                  Terry D. Eppler
+      Created:                 05-31-2023
+
+      Last Modified By:        Terry D. Eppler
+      Last Modified On:        06-01-2023
+  ******************************************************************************************
+  <copyright file="main.py" company="Terry D. Eppler">
+
+     Boo is an ai powered data analysis tool for EPA Analysts.
+     Copyright ©  2024  Terry Eppler
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the “Software”),
+     to deal in the Software without restriction,
+     including without limitation the rights to use,
+     copy, modify, merge, publish, distribute, sublicense,
+     and/or sell copies of the Software,
+     and to permit persons to whom the Software is furnished to do so,
+     subject to the following conditions:
+
+     The above copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+     INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+     DEALINGS IN THE SOFTWARE.
+
+     You can contact me at:  terryeppler@gmail.com or eppler.terry@epa.gov
+
+  </copyright>
+  <summary>
+    boo.py
+  </summary>
+  ******************************************************************************************
+  '''
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
