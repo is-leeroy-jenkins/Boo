c7fadd8244410d4f35cadcf88eaf47e6319ef30b
Terry D. Eppler <terry.eppler@gmail.com>
2025-06-15 12:34:37 -0400

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 '''
   ******************************************************************************************
       Assembly:                Boo
-      Filename:                main.py
+      Filename:                app.py
       Author:                  Terry D. Eppler
       Created:                 05-31-2022
 
@@ -38,32 +38,59 @@
 
   </copyright>
   <summary>
-    main.py
+    app.py
   </summary>
   ******************************************************************************************
-  '''
+'''
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
 
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 
-def main( ) -> None:
-    """
+app = Dash( )
 
-        Main function. Initialize the window and handle the events.
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
 
-    """
-    window = sg.Window( 'Hello World!' )
-    logger.debug( 'Application started.' )
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame( {
+    'Fruit': [ 'Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas' ],
+    'Amount': [ 4, 1, 2, 2, 4, 5 ],
+    'City': [ 'SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal' ]
+})
 
-    while True:
-        event: str
-        values: Dict[ str, Any ]
-        event, values = window.read( )
+fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode='group')
 
-        if event in [ '-CLOSE_BUTTON-', sg.WIN_CLOSED ]:
+fig.update_layout(
+    plot_bgcolor=colors[ 'background' ],
+    paper_bgcolor=colors[ 'background' ],
+    font_color=colors['text']
+)
 
-            logger.debug( 'Closing...' )
-            break
-    window.close( )
+app.layout = html.Div( style={ 'backgroundColor': colors['background' ]}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors[ 'text' ]
+        }
+    ),
 
+    html.Div( children='Dash: A web application framework for your data.', style={
+        'textAlign': 'center',
+        'color': colors[ 'text' ]
+    }),
 
-if __name__ == "__main__":
-    main( )
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run( debug=True )
