eaa0bf8266b17a6d196bece16a69e3bc7f6c599e
teppler <terryeppler@gmail.com>
2025-10-30 11:58:47 -0400

    update

--- agents.py
+++ agents.py
@@ -9584,7 +9584,7 @@ class BusinessResearcher( Agent ):
 		self.model = 'gpt-5-nano-2025-08-07'
 		self.tool_choice = 'auto'
 		self.id = 'pmpt_68655ad6ea6881909b7d348866af27910beeb7966664cf8f'
-		self.version = '5'
+		self.version = '7'
 		self.format = 'text'
 		self.reasoning = { 'effort': 'medium' }
 		self.include = [ 'reasoning.encrypted_content', 'web_search_call.action.sources' ]
@@ -9630,7 +9630,7 @@ class BusinessResearcher( Agent ):
 			error = ErrorDialog( exception )
 			error.show( )
 
-class NewAnalyst( Agent ):
+class BusinessPlanner( Agent ):
 	'''
 
 
@@ -9671,8 +9671,8 @@ class NewAnalyst( Agent ):
 		self.client.api_key = os.getenv( 'OPENAI_API_KEY' )
 		self.model = 'gpt-5-nano-2025-08-07'
 		self.tool_choice = 'auto'
-		self.id = ''
-		self.version = ''
+		self.id = 'pmpt_68655a8e8e9881908f858f54c361bb760e2c93d271d3125a'
+		self.version = '4'
 		self.format = 'text'
 		self.reasoning = { 'effort': 'medium' }
 		self.include = [ 'reasoning.encrypted_content', 'web_search_call.action.sources' ]
@@ -9696,32 +9696,8 @@ class NewAnalyst( Agent ):
 		try:
 			throw_if( 'question', question )
 			self.question = question
-			search_tool = {
-					'type': 'file_search',
-					'vector_store_ids': self.vector_store_ids }
-			files = {
-					'type': 'auto',
-					'file_ids': self.file_ids }
-			code_tool = {
-					'type': 'code_interpreter',
-					'container': files }
-			self.tools.append( search_tool )
-			self.tools.append( code_tool )
-			variable = {
-					'question': self.question }
-			meta = {
-					'id': self.id,
-					'version': self.version,
-					'variables': variable }
-			search_tool = {
-					'type': 'file_search',
-					'vector_store_ids': self.vector_store_ids }
-			container = {
-					'type': 'auto',
-					'file_ids': self.file_ids }
-			code_tool = {
-					'type': 'code_interpreter',
-					'container': container }
+			variable = { 'question': self.question }
+			meta = { 'id': self.id, 'version': self.version, 'variables': variable }
 			response = self.client.responses.create( model=self.model, prompt=meta, store=self.store,
 				max_output_tokens=self.max_output_tokens, include=self.include, reasoning=self.reasoning )
 			output = _response.output_text
@@ -9729,7 +9705,96 @@ class NewAnalyst( Agent ):
 		except Exception as e:
 			exception = Error( e )
 			exception.module = 'agents'
-			exception.cause = ''
+			exception.cause = 'BusinessPlanner'
+			exception.method = 'ask( self, question: str ) -> str | None'
+			error = ErrorDialog( exception )
+			error.show( )
+
+class BusinessAnalyst( Agent ):
+	'''
+
+
+		Purpose:
+		--------
+
+	'''
+	client: Optional[ OpenAI ]
+	model: Optional[ str ]
+	prompt: Optional[ Prompt ]
+	reasoning: Optional[ Dict[ str, str ] ]
+	text: Optional[ Text ]
+	format: Optional[ str ]
+	max_output_tokens: Optional[ int ]
+	input: Optional[ List ]
+	store: Optional[ bool ]
+	temperature: Optional[ float ]
+	top_p: Optional[ float ]
+	tools: Optional[ List[ Dict[ str, str ] ] ]
+	include: Optional[ List ]
+	question: Optional[ str ]
+	variables: Optional[ List[ str ] ]
+	include: Optional[ List[ str ] ]
+	tool_choice: Optional[ str ]
+	vector_store_ids: Optional[ List[ str ] ]
+	file_ids: Optional[ List[ str ] ]
+	tool_choice: Optional[ str ]
+	ticker: Optional[ str ]
+	company: Optional[ str ]
+	sector: Optional[ str ]
+	
+	def __init__( self ):
+		'''
+
+			Purpose:
+			-------
+			Contructor for class objects
+
+		'''
+		self.client = OpenAI( )
+		self.client.api_key = os.getenv( 'OPENAI_API_KEY' )
+		self.model = 'gpt-5-nano-2025-08-07'
+		self.tool_choice = 'auto'
+		self.id = 'pmpt_68655a41a1748196a7c864aaee0af331041858ced4469344'
+		self.version = '3'
+		self.format = 'text'
+		self.reasoning = { 'effort': 'medium' }
+		self.include = [ 'reasoning.encrypted_content', 'web_search_call.action.sources' ]
+		self.tools = [ ]
+		self.file_ids = [ ]
+		self.vector_store_ids = [ ]
+	
+	def ask( self, question: str, ticker: str, company: str, sector: str ) -> str | None:
+		'''
+
+			Purpose:
+			-------
+
+			Parameters:
+			-----------
+
+			Returns:
+			---------
+
+		'''
+		try:
+			throw_if( 'question', question )
+			throw_if( 'ticker', ticker )
+			throw_if( 'company', company )
+			throw_if( 'sector', sector )
+			self.question = question
+			self.ticker = ticker
+			self.company = company
+			self.sector = sector
+			variable = { 'question': self.question }
+			meta = { 'id': self.id, 'version': self.version, 'variables': variable }
+			response = self.client.responses.create( model=self.model, prompt=meta, store=self.store,
+				max_output_tokens=self.max_output_tokens, include=self.include, reasoning=self.reasoning )
+			output = _response.output_text
+			return output
+		except Exception as e:
+			exception = Error( e )
+			exception.module = 'agents'
+			exception.cause = 'BusinessAnalyst'
 			exception.method = 'ask( self, question: str ) -> str | None'
 			error = ErrorDialog( exception )
 			error.show( )
